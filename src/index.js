8dba7aa6ad6f04d17a05393b45a71fe30a485cdc
surehman360 <130796096+surehman360@users.noreply.github.com>
2024-09-25 22:22:50 +0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import reactDOM from "react-dom";
+
+reactDOM.render(
+  <div>
+    <h1>Hello World!</h1>
+    <ul>
+      <li>Banana</li>
+      <li>Orange</li>
+      <li>Apple</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
+//Create a react app from scratch.
+//It should display a h1 heading.
+//It should display an unordered list (bullet points).
+//It should contain 3 list elements.
